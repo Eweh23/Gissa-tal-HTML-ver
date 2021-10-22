18c6880075530a8442624cf71cd9acf92bb80d1d
Eweh23 <92167640+Eweh23@users.noreply.github.com>
2021-10-22 16:54:59 +0200

    Commit

--- myscript.js
+++ myscript.js
@@ -1,20 +1,20 @@
-var slumpran = Math.floor(Math.random() * 100) + 1;
-var val = document.getElementById("gissning");
-var elementtxt1 = document.getElementById("txt1");
+var svar = Math.floor(Math.random() * 100) + 1;
+var input = document.getElementById("gissning");
+var text = document.getElementById("hint");
 var count = 0;
 
-function slump(){
+function func(){
     count ++;
 
-        if(val.value < slumpran){
-            elementtxt1.innerHTML="Too low";
+        if(input.value < svar){
+            text.innerHTML="Too low";
             
         }
-        else if(val.value > slumpran){
-            elementtxt1.innerHTML="Too high";
+        else if(input.value > svar){
+            text.innerHTML="Too high";
         }
         else{
-            elementtxt1.innerHTML="Correct!";
+            text.innerHTML="Correct!";
         }
         document.getElementById("txtcount").innerHTML="Count: " + count;
 }
